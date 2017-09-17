c80be065e48ed62c80cc73a9cac57caa773f2f79
Alex <alexander.komischke@stud.uni-hannover.de>
2017-09-17 18:59:55 +0200

    Added basic files for a new stage, where you can select a player, to delete it.

--- source/src/gui/fxml_controller/MainGuiController.java
+++ source/src/gui/fxml_controller/MainGuiController.java
@@ -2,6 +2,7 @@ package gui.fxml_controller;
 
 import gui.fxml_controller.enemy.AddEnemyController;
 import gui.fxml_controller.player.AddPlayerController;
+import gui.fxml_controller.player.RemovePlayerController;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -24,7 +25,7 @@ public class MainGuiController {
     //---------------------------------------- FXML MEMBERS ----------------------------------------
 
 
-    @FXML Button addPlayerButton, removePlayerButton, startButton, addEnemyButton;
+    @FXML Button addPlayerButton, removePlayerButton, startButton, addEnemyButton, removeEnemyButton;
 
     @FXML TextField iterationsField;
 
@@ -33,13 +34,16 @@ public class MainGuiController {
     //FXML Controller
     @FXML private AddPlayerController playerCon;
     @FXML private AddEnemyController enemyCon;
+    @FXML private RemovePlayerController removePlayerController;
 
     //------------------------------------------ MEMBERS ------------------------------------------
 
     private Battle battle; // Contains the simulation
 
     private Stage addPlayerStage;
+    private Stage removePlayerStage;
     private Stage addEnemyStage;
+    private Stage removeEnemyStage;
 
 
     //---------------------------------------- FXML METHODS ----------------------------------------
@@ -81,6 +85,7 @@ public class MainGuiController {
         //System.out.println("Starting Battle : " + this.getBattle());
         this.getBattle().run();
         this.summaryField.setText(this.getBattle().getSummary());
+        System.out.println(summaryField.getText());
 
     }
 
@@ -91,12 +96,17 @@ public class MainGuiController {
 
     @FXML
     public void removePlayerButtonPressed(){
-        
+        //this.removePlayerStage.show();
     }
 
     @FXML
     public void addEnemyButtonClicked() {
         this.addEnemyStage.show();
+    }
+
+    @FXML
+    public void removeEnemyButtonPressed(){
+
     }
 
 
@@ -108,6 +118,8 @@ public class MainGuiController {
     private void createStages() {
         createPlayerStage();
         createEnemyStage();
+        //createPlayerRemoveStage();
+        //createEnemyRemoveStage();
     }
 
     private void createPlayerStage() {
@@ -165,6 +177,47 @@ public class MainGuiController {
         );
     }
 
+    private void createPlayerRemoveStage(){
+        FXMLLoader loader = new FXMLLoader(AddPlayerController.class.getResource("../../fxml/player/RemovePlayer.fxml"));
+
+        try {
+            Parent root = loader.load();
+            removePlayerController = loader.getController();
+            Scene s = new Scene(root);
+            removePlayerStage = new Stage();
+            removePlayerStage.setScene(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        removePlayerStage.setTitle("Remove a Player");
+
+
+        removePlayerStage.setOnHiding((WindowEvent event) -> Platform.runLater(() -> playerListField.setText(
+                "Remove Player does not work currently."
+        )));
+    }
+
+    private void createEnemyRemoveStage(){
+        FXMLLoader loader = new FXMLLoader(AddEnemyController.class.getResource("../../fxml/enemy/RemoveEnemy.fxml"));
+
+        try {
+            Parent root = loader.load();
+            enemyCon = loader.getController();
+            Scene s = new Scene(root);
+            addEnemyStage = new Stage();
+            addEnemyStage.setScene(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        addEnemyStage.setTitle("Remove an Enemy");
+
+        addEnemyStage.setOnHiding((WindowEvent event) -> Platform.runLater(() -> enemyListField.setText(
+                enemyCon.enemyListAsString()
+        )));
+    }
+
     private void initialize_TextFields() {
         this.setFieldToOnlyNumbers(this.iterationsField);
         this.iterationsField.setText("0");
@@ -191,5 +244,21 @@ public class MainGuiController {
 
     public void setBattle(Battle battle) {
         this.battle = battle;
+    }
+
+    public RemovePlayerController getRemovePlayerController() {
+        return removePlayerController;
+    }
+
+    public void setRemovePlayerController(RemovePlayerController removePlayerController) {
+        this.removePlayerController = removePlayerController;
+    }
+
+    public Stage getRemovePlayerStage() {
+        return removePlayerStage;
+    }
+
+    public void setRemovePlayerStage(Stage removePlayerStage) {
+        this.removePlayerStage = removePlayerStage;
     }
 }
---
+++ source/src/gui/fxml_controller/player/RemovePlayerController.java
@@ -0,0 +1,112 @@
+package gui.fxml_controller.player;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.VBox;
+import model.player.Player;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class RemovePlayerController {
+
+    @FXML
+    Button removeButton, cancelButton;
+
+    @FXML
+    VBox PlayerBox;
+
+    private List<CheckBox> checkboxes;
+
+    @FXML
+    public void initialize() {
+        this.checkboxes = new LinkedList<>();
+    }
+
+
+    @FXML
+    public void removeButtonPressed(){}
+
+    @FXML
+    public void cancelButtonPressed(){
+        cancelButton.getScene().getWindow().hide();
+    }
+
+    public void addToCheckboxes(Player player) {
+        if(!this.checkboxes.contains(new CheckBox(player.getName()))){
+            CheckBox c = new CheckBox(player.getName());
+            c.setSelected(false);
+            c.selectedProperty().addListener((observable, oldValue, newValue) -> {
+                deselectOtherBoxes(c);
+                c.setSelected(newValue);
+            });
+            this.checkboxes.add(c);
+            PlayerBox.getChildren().add(c);
+        }
+        else{
+            System.out.println("PlayerBoxController : addToCheckbox : Already contains this player.");
+        }
+    }
+
+    public void updateCheckboxes(List<Player> list) {
+        this.checkboxes = new LinkedList<>();
+
+        for (Player p : list) {
+            CheckBox c = new CheckBox(p.getName());
+            c.setSelected(false);
+            c.selectedProperty().addListener((observable, oldValue, newValue) -> {
+                deselectOtherBoxes(c);
+                c.setSelected(newValue);
+            });
+
+            this.checkboxes.add(c);
+        }
+        PlayerBox.getChildren().clear();
+        PlayerBox.getChildren().addAll(checkboxes);
+
+    }
+
+    //--------------------------------- PRIVATE METHODS --------------------------------
+
+    private void deselectOtherBoxes(CheckBox checky){
+        for(CheckBox c : this.getCheckboxes()){
+            if(!c.equals(checky)){
+                c.setSelected(false);
+            }
+        }
+    }
+
+    private boolean isCheckBoxSelectable(CheckBox checky){
+
+        for(CheckBox c : this.getCheckboxes()){
+            if(!c.equals(checky)) {
+                if (c.isSelected()) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    //--------------------------------- GETTER AND SETTER -------------------------------
+
+    public List<CheckBox> getCheckboxes() {
+        return checkboxes;
+    }
+
+    public void setCheckboxes(List<CheckBox> checkboxes) {
+        this.checkboxes = checkboxes;
+    }
+
+    public String getSelected() {
+        for(CheckBox c : this.checkboxes){
+            if(c.isSelected()){
+                return c.getText();
+            }
+        }
+        return null;
+    }
+
+}
